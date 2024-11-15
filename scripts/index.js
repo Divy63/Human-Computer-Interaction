d285a78ad61337f8fd920dfe56e18c5e80be5332
Ian Gilhuly <gilhuly@gmail.com>
2024-11-14 20:55:22 -0600

    Added click functionslity and fixed open list issue

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(document).click(function(event) {
+        if(
+          $('.toggle > input').is(':checked') &&
+          !$(event.target).parents('.toggle').is('.toggle')
+        ) {
+          $('.toggle > input').prop('checked', false);
+        }
+      });
+   
+  });
