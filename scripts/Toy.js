d34ae8affa4947d63ad7976d78519495cba3a604
Divy Patel <divyp630@gmail.com>
2024-11-18 19:31:24 -0600

    Changed the way the toys were getting printed to make it easier to filter

--- scripts/Toy.js
+++ scripts/Toy.js
@@ -1,13 +1,14 @@
 class Toy{
 
     // Constructor for a toy class
-    constructor(name,price,rating,quantity,imageLink,otherLink){
+    constructor(name,price,rating,quantity,imageLink,otherLink,toyType){
         this.name=name;
         this.price=price;
         this.rating=rating;
         this.quantity=quantity;
         this.imageLink=imageLink;
         this.otherLink=otherLink;
+        this.toyType=toyType;
     }
 
 
