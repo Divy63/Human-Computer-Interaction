4ca36c4908bfbc5aca20e2720b4c06c52dc09552
Divy Patel <divyp630@gmail.com>
2024-11-22 17:59:08 -0600

    FInished search

---
+++ scripts/categories.js
@@ -0,0 +1,135 @@
+// -------------------------------------------------------------------------------------------------------------------------------------
+// Categories button listeners
+// -------------------------------------------------------------------------------------------------------------------------------------
+//Display Toy Cards for Categories Added listeners for categories
+let booksAndComicsButton = document.getElementById("BooksAndComicsButton").addEventListener("click", function () {
+    showBooksAndComics();
+    let toyCardArea = document.getElementById("box-toy-card");
+    // Scrolls to toy card section
+    window.scrollTo({
+        top: toyCardArea.offsetTop,
+        behavior: "smooth",
+    });
+
+    $("#t4").removeAttr('disabled');
+    $("label.t4").css("color", "black");
+    $("#electronics-cat1").hide();
+    $("#electronics-cat2").hide();
+    $("#electronics-cat3").hide();
+    $("#action-cat1").hide();
+    $("#action-cat2").hide();
+    $("#action-cat3").hide();
+    $("#game-cat1").hide();
+    $("#game-cat2").hide();
+    $("#game-cat3").hide();
+    $("#outdoor-cat1").hide();
+    $("#outdoor-cat2").hide();
+    $("#outdoor-cat3").hide();
+    $("#book-cat1").show();
+    $("#book-cat2").show();
+    $("#book-cat3").show();
+});
+let outdoorButton = document.getElementById("outdoorButton").addEventListener("click", function () {
+    showOutdoor();
+    let toyCardArea = document.getElementById("box-toy-card");
+    // Scrolls to toy card section
+    window.scrollTo({
+        top: toyCardArea.offsetTop,
+        behavior: "smooth",
+    });
+    $("#t4").removeAttr('disabled');
+    $("label.t4").css("color", "black");
+    $("#electronics-cat1").hide();
+    $("#electronics-cat2").hide();
+    $("#electronics-cat3").hide();
+    $("#action-cat1").hide();
+    $("#action-cat2").hide();
+    $("#action-cat3").hide();
+    $("#game-cat1").hide();
+    $("#game-cat2").hide();
+    $("#game-cat3").hide();
+    $("#book-cat1").hide();
+    $("#book-cat2").hide();
+    $("#book-cat3").hide();
+    $("#outdoor-cat1").show();
+    $("#outdoor-cat2").show();
+    $("#outdoor-cat3").show();
+});
+let electronicsButton = document.getElementById("electronicsButton").addEventListener("click", function () {
+    showElectronics();
+    let toyCardArea = document.getElementById("box-toy-card");
+    // Scrolls to toy card section
+    window.scrollTo({
+        top: toyCardArea.offsetTop,
+        behavior: "smooth",
+    });
+    $("#t4").removeAttr('disabled');
+    $("label.t4").css("color", "black");
+    $("#book-cat1").hide();
+    $("#book-cat2").hide();
+    $("#book-cat3").hide();
+    $("#action-cat1").hide();
+    $("#action-cat2").hide();
+    $("#action-cat3").hide();
+    $("#game-cat1").hide();
+    $("#game-cat2").hide();
+    $("#game-cat3").hide();
+    $("#outdoor-cat1").hide();
+    $("#outdoor-cat2").hide();
+    $("#outdoor-cat3").hide();
+    $("#electronics-cat1").show();
+    $("#electronics-cat2").show();
+    $("#electronics-cat3").show();
+});
+let GamesAndPuzzlesButton = document.getElementById("GamesAndPuzzlesButton").addEventListener("click", function () {
+    showGamesAndPuzzles();
+    let toyCardArea = document.getElementById("box-toy-card");
+    // Scrolls to toy card section
+    window.scrollTo({
+        top: toyCardArea.offsetTop,
+        behavior: "smooth",
+    });
+    $("#t4").removeAttr('disabled');
+    $("label.t4").css("color", "black");
+    $("#electronics-cat1").hide();
+    $("#electronics-cat2").hide();
+    $("#electronics-cat3").hide();
+    $("#action-cat1").hide();
+    $("#action-cat2").hide();
+    $("#action-cat3").hide();
+    $("#book-cat1").hide();
+    $("#book-cat2").hide();
+    $("#book-cat3").hide();
+    $("#outdoor-cat1").hide();
+    $("#outdoor-cat2").hide();
+    $("#outdoor-cat3").hide();
+    $("#game-cat1").show();
+    $("#game-cat2").show();
+    $("#game-cat3").show();
+});
+let ActionFigures = document.getElementById("ActionFiguresButton").addEventListener("click", function () {
+    showActionFigures();
+    let toyCardArea = document.getElementById("box-toy-card");
+    // Scrolls to toy card section
+    window.scrollTo({
+        top: toyCardArea.offsetTop,
+        behavior: "smooth",
+    });
+    $("#t4").removeAttr('disabled');
+    $("label.t4").css("color", "black");
+    $("#electronics-cat1").hide();
+    $("#electronics-cat2").hide();
+    $("#electronics-cat3").hide();
+    $("#book-cat1").hide();
+    $("#book-cat2").hide();
+    $("#book-cat3").hide();
+    $("#game-cat1").hide();
+    $("#game-cat2").hide();
+    $("#game-cat3").hide();
+    $("#outdoor-cat1").hide();
+    $("#outdoor-cat2").hide();
+    $("#outdoor-cat3").hide();
+    $("#action-cat1").show();
+    $("#action-cat2").show();
+    $("#action-cat3").show();
+});
---
+++ scripts/search.js
@@ -0,0 +1,63 @@
+searchBar=document.getElementById('searchBar').addEventListener('input',function(){
+    const searchText=this.value.trim();
+    const resultsDiv=document.getElementById('result');
+    const searchButton=document.getElementById("searchButton");
+    resultsDiv.innerHTML='';//clear previous 
+    let toys=new Toys();
+    if(searchText.length>0){
+        const results=TOY_DATABASE.searchToys(searchText);
+        console.log(results);
+        if(results.length>0){
+            results.forEach(toy=>{
+                console.log(toy);
+                const toyResultDiv=document.createElement('div');
+                toyResultDiv.className='result-item'
+                toyResultDiv.textContent=`${toy.getName()} (Age:${toy.getAge()}+) Price: ${toy.getPrice()}`;
+
+                searchButton.addEventListener('click',()=>{
+                    toys=new Toys();
+                    for(let i=0;i<results.length;i++){
+                        toys.add(results[i]);
+                    }
+                    toys.displayHTML();
+                    let toyCardArea = document.getElementById("box-toy-card");
+                    // Scrolls to toy card section
+                    window.scrollTo({
+                        top: toyCardArea.offsetTop,
+                        behavior: "smooth",
+                    });
+
+                    resultsDiv.innerHTML='';
+                    
+
+                })
+                toyResultDiv.addEventListener('click',()=>{
+                    toys=new Toys();
+                    toys.add(toy);
+                    toys.displayHTML();
+                    let toyCardArea = document.getElementById("box-toy-card");
+                    // Scrolls to toy card section
+                    window.scrollTo({
+                        top: toyCardArea.offsetTop,
+                        behavior: "smooth",
+                    });
+                    resultsDiv.innerHTML='';
+                    document.getElementById('searchBar').value=toy.getName();
+
+                });
+
+                resultsDiv.appendChild(toyResultDiv);
+                console.log(resultsDiv);
+
+            });
+        }
+    }else{
+        const noResultDiv=document.createElement('div');
+        noResultDiv.className='result-item'
+        noResultDiv.textContent='No results found.';
+        resultsDiv.appendChild(noResultDiv);
+    }
+
+});
+
+
