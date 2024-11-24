4d460446caf9dcfc2183ea0aff802408840fa17e
Ian Gilhuly <gilhuly@gmail.com>
2024-11-24 17:13:10 -0600

    Streamlined filters

--- scripts/Toys.js
+++ scripts/Toys.js
@@ -2,6 +2,7 @@
 class Toys{
     constructor(){
         this.toys = [];//Array that stores a toy
+        this.filter = ["Category","None","Age",0,Infinity,"Price",0,Infinity,"STAR",6];//applied filters
     }
 
     // Method that adds toy to the List
@@ -16,6 +17,24 @@ class Toys{
 
     get(i){
         return this.toys[i];
+
+    }
+
+    editFilterArray(type,start,end=Infinity){
+        if(type === "Category"){
+            this.filter[1] = start;            
+        }
+        if(type === "Age"){
+            this.filter[3] = start;
+            this.filter[4] = end;            
+        }
+        if(type === "Price"){
+            this.filter[6] = start;
+            this.filter[7] = end;            
+        }
+        if(type === "Star"){
+            this.filter[9] = start;                     
+        }
 
     }
 
@@ -124,6 +143,8 @@ class Toys{
 
     applyPriceFilter(start,end=Infinity){
         console.log("In Price FIlter");
+        console.log("current db");
+       console.log(this.toys);
         let filteredToys=new Toys();
         for(let i=0;i<this.toys.length;i++){
             console.log("IN LOOP");
@@ -135,11 +156,14 @@ class Toys{
             }
         }
         filteredToys.displayHTML();
-
+        
+        
     }   
 
     applyAgeFilter(start,end=Infinity){
         console.log("In Price FIlter");
+        console.log("current db");
+        console.log(this.toys);
         let filteredToys=new Toys();
         for(let i=0;i<this.toys.length;i++){
             let toy=this.toys[i];
@@ -150,11 +174,17 @@ class Toys{
             }
         }
         filteredToys.displayHTML();
+       
 
     }   
 
     applyRatingFilter(star){
         console.log("In Rating FIlter");
+        this.filterArray[2] = category;
+       console.log("Filter array");
+       console.log(this.filterArray);
+        console.log("current db");
+       console.log(this.toys);
         let filteredToys=new Toys();
         for(let i=0;i<this.toys.length;i++){
             let toy=this.toys[i];
@@ -165,9 +195,81 @@ class Toys{
             }
         }
         filteredToys.displayHTML();
-
+        
     }
 
+    applyCategoryFilter(category){
+        //let htmlContainer=document.getElementById('box-toy-card');
+       // htmlContainer.innerHTML='';//Emptying the container
+       this.filterArray[2] = category;
+       console.log("Filter array");
+       console.log(this.filterArray);
+       console.log("current db");
+       console.log(this.toys);
+       //alert(category)
+        let filteredToys=new Toys();
+        for(let i=0;i<this.toys.length;i++){
+            let toy=this.toys[i];
+            if(toy.getToyType()===category){
+                console.log("FILTERED");
+                filteredToys.add(this.toys[i]);
+            }
+        }
+        filteredToys.displayHTML();
+        
+    }
+
+    filterToyDB(){
+       
+        let filteredToys=new Toys();        
+        for(let i=0;i<this.toys.length;i++){
+            let toy=this.toys[i];
+            let price= parseFloat(this.toys[i].getPrice().replace('$', ''));
+            if (this.filter[1] === "None"){ 
+                console.log("filter by none");               
+                if (this.filter[9] <= 5){
+                    if (toy.getRating()===this.filter[9]){
+                        if (price>=this.filter[6] && price<=this.filter[7]){
+                            if(toy.getAge()>=this.filter[3] && toy.getAge()<=this.filter[4]){
+                                filteredToys.add(this.toys[i]);
+                            }
+                        }
+                    }     
+                } 
+                else{
+                    if (price>=this.filter[6] && price<=this.filter[7]){
+                        if(toy.getAge()>=this.filter[3] && toy.getAge()<=this.filter[4]){
+                            filteredToys.add(this.toys[i]);
+                        }
+                    }
+                }      
+            }
+            else{
+                console.log("filter by category");
+                if(toy.getToyType()===this.filter[1])
+                {  
+                    if (this.filter[9] <= 5){
+                        if (toy.getRating()===this.filter[9]){
+                            if (price>=this.filter[6] && price<=this.filter[7]){
+                                if(toy.getAge()>=this.filter[3] && toy.getAge()<=this.filter[4]){
+                                    filteredToys.add(this.toys[i]);
+                                }
+                            }
+                        }     
+                    } 
+                    else{
+                        if (price>=this.filter[6] && price<=this.filter[7]){
+                            if(toy.getAge()>=this.filter[3] && toy.getAge()<=this.filter[4]){
+                                filteredToys.add(this.toys[i]);
+                            }
+                        }
+                    }                    
+                }
+            }    
+        }
+        filteredToys.displayHTML();
+
+    }
 
     searchToys(searchText){
 
--- scripts/filter.js
+++ scripts/filter.js
@@ -6,7 +6,11 @@
 // Get the dropdown container by its ID
 let price0to10 = document.getElementById("price0to10").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyPriceFilter(0, 10);
+    //TOY_DATABASE.applyPriceFilter(0, 10); 
+    TOY_DATABASE.editFilterArray("Price",0,10);
+    TOY_DATABASE.filterToyDB();
+    //filterArray[3] = "0-10";
+    //TOY_DATABASE.filterto(filterArray);   
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -30,7 +34,9 @@ let price0to10 = document.getElementById("price0to10").addEventListener("click",
 
 let price10to20 = document.getElementById("price10to20").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyPriceFilter(10, 20);
+    //TOY_DATABASE = TOY_DATABASE.applyPriceFilter(10, 20);
+    TOY_DATABASE.editFilterArray("Price",10,20);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -53,7 +59,9 @@ let price10to20 = document.getElementById("price10to20").addEventListener("click
 
 let price20to30 = document.getElementById("price20to30").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyPriceFilter(20, 30);
+    //TOY_DATABASE = TOY_DATABASE.applyPriceFilter(20, 30);
+    TOY_DATABASE.editFilterArray("Price",20,30);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -74,7 +82,9 @@ let price20to30 = document.getElementById("price20to30").addEventListener("click
 
 let price30to40 = document.getElementById("price30to40").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyPriceFilter(30, 40);
+    //TOY_DATABASE = TOY_DATABASE.applyPriceFilter(30, 40);
+    TOY_DATABASE.editFilterArray("Price",30,40);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -96,7 +106,9 @@ let price30to40 = document.getElementById("price30to40").addEventListener("click
 
 let priceMoreThan40 = document.getElementById("priceMoreThan40").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyPriceFilter(40);
+    //TOY_DATABASE = TOY_DATABASE.applyPriceFilter(40);
+    TOY_DATABASE.editFilterArray("Price",40,Infinity);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -119,7 +131,9 @@ let priceMoreThan40 = document.getElementById("priceMoreThan40").addEventListene
 
 let undoPriceFilter = document.getElementById("priceUndo").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyPriceFilter(0);
+    //TOY_DATABASE = TOY_DATABASE.applyPriceFilter(0);
+    TOY_DATABASE.editFilterArray("Price",0,Infinity);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -142,7 +156,9 @@ let undoPriceFilter = document.getElementById("priceUndo").addEventListener("cli
 // Age Filters
 let age0to2 = document.getElementById("age0to2").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyAgeFilter(0, 2);
+    //TOY_DATABASE = TOY_DATABASE.applyAgeFilter(0, 2);
+    TOY_DATABASE.editFilterArray("Age",0,2);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -165,7 +181,9 @@ let age0to2 = document.getElementById("age0to2").addEventListener("click", (even
 
 let age2to5 = document.getElementById("age2to5").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyAgeFilter(2, 5);
+    //TOY_DATABASE = TOY_DATABASE.applyAgeFilter(2, 5);
+    TOY_DATABASE.editFilterArray("Age",2,5);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -188,7 +206,9 @@ let age2to5 = document.getElementById("age2to5").addEventListener("click", (even
 
 let age5to10 = document.getElementById("age5to10").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyAgeFilter(5, 10);
+    //TOY_DATABASE = TOY_DATABASE.applyAgeFilter(5, 10);
+    TOY_DATABASE.editFilterArray("Age",5,10);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -210,7 +230,9 @@ let age5to10 = document.getElementById("age5to10").addEventListener("click", (ev
 
 let ageMoreThan10 = document.getElementById("ageMoreThan10").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyAgeFilter(10);
+    //TOY_DATABASE = TOY_DATABASE.applyAgeFilter(10);
+    TOY_DATABASE.editFilterArray("Age",10,Infinity);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -232,7 +254,9 @@ let ageMoreThan10 = document.getElementById("ageMoreThan10").addEventListener("c
 
 let undoAgeFilter = document.getElementById("ageUndo").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyAgeFilter(0);
+    //TOY_DATABASE = TOY_DATABASE.applyAgeFilter(0);
+    TOY_DATABASE.editFilterArray("Age",0,Infinity);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -256,7 +280,9 @@ let undoAgeFilter = document.getElementById("ageUndo").addEventListener("click",
 // Rating Filter
 let oneStar = document.getElementById("oneStar").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyRatingFilter(1);
+    //TOY_DATABASE = TOY_DATABASE.applyRatingFilter(1);
+    TOY_DATABASE.editFilterArray("Star",1);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -278,7 +304,9 @@ let oneStar = document.getElementById("oneStar").addEventListener("click", (even
 
 let twoStar = document.getElementById("twoStar").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyRatingFilter(2);
+    //TOY_DATABASE = TOY_DATABASE.applyRatingFilter(2);
+    TOY_DATABASE.editFilterArray("Star",2);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -300,7 +328,9 @@ let twoStar = document.getElementById("twoStar").addEventListener("click", (even
 
 let threeStar = document.getElementById("threeStar").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyRatingFilter(3);
+    //TOY_DATABASE = TOY_DATABASE.applyRatingFilter(3);
+    TOY_DATABASE.editFilterArray("Star",3);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -322,7 +352,9 @@ let threeStar = document.getElementById("threeStar").addEventListener("click", (
 
 let fourStar = document.getElementById("fourStar").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyRatingFilter(4);
+    //TOY_DATABASE = TOY_DATABASE.applyRatingFilter(4);
+    TOY_DATABASE.editFilterArray("Star",4);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -344,7 +376,11 @@ let fourStar = document.getElementById("fourStar").addEventListener("click", (ev
 
 let fiveStar = document.getElementById("fiveStar").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyRatingFilter(5);
+    //TOY_DATABASE = TOY_DATABASE.applyRatingFilter(5);
+    TOY_DATABASE.editFilterArray("Star",5);
+    TOY_DATABASE.filterToyDB();
+    filterArray.push("Star");
+    filterArray.push("");
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
@@ -366,7 +402,9 @@ let fiveStar = document.getElementById("fiveStar").addEventListener("click", (ev
 
 let undoStarFilter = document.getElementById("ratingUndo").addEventListener("click", (event) => {
     event.preventDefault();
-    TOY_DATABASE.applyAgeFilter(6);
+    //TOY_DATABASE = TOY_DATABASE.applyAgeFilter(6);
+    TOY_DATABASE.editFilterArray("Star",6);
+    TOY_DATABASE.filterToyDB();
     let toyCardArea = document.getElementById("box-toy-card");
     // Scrolls to toy card section
     window.scrollTo({
