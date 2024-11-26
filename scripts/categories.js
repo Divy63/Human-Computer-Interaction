d20b52634f68b2c18f2a128035d0d6b2c9e31285
Ian Gilhuly <gilhuly@gmail.com>
2024-11-26 15:07:05 -0600

    Removed Subcategories

--- scripts/categories.js
+++ scripts/categories.js
@@ -25,23 +25,7 @@ let booksAndComicsButton = document.getElementById("BooksAndComicsButton").addEv
     document.getElementById("electronicsButton").style.backgroundColor = "#03a359"; 
     //end
 
-    $("#t4").removeAttr('disabled');
-    $("label.t4").css("color", "black");
-    $("#electronics-cat1").hide();
-    $("#electronics-cat2").hide();
-    $("#electronics-cat3").hide();
-    $("#action-cat1").hide();
-    $("#action-cat2").hide();
-    $("#action-cat3").hide();
-    $("#game-cat1").hide();
-    $("#game-cat2").hide();
-    $("#game-cat3").hide();
-    $("#outdoor-cat1").hide();
-    $("#outdoor-cat2").hide();
-    $("#outdoor-cat3").hide();
-    $("#book-cat1").show();
-    $("#book-cat2").show();
-    $("#book-cat3").show();
+    
 });
 let outdoorButton = document.getElementById("outdoorButton").addEventListener("click", function () {
     
@@ -64,23 +48,7 @@ let outdoorButton = document.getElementById("outdoorButton").addEventListener("c
     document.getElementById("electronicsButton").style.backgroundColor = "#03a359"; 
     //end
 
-    $("#t4").removeAttr('disabled');
-    $("label.t4").css("color", "black");
-    $("#electronics-cat1").hide();
-    $("#electronics-cat2").hide();
-    $("#electronics-cat3").hide();
-    $("#action-cat1").hide();
-    $("#action-cat2").hide();
-    $("#action-cat3").hide();
-    $("#game-cat1").hide();
-    $("#game-cat2").hide();
-    $("#game-cat3").hide();
-    $("#book-cat1").hide();
-    $("#book-cat2").hide();
-    $("#book-cat3").hide();
-    $("#outdoor-cat1").show();
-    $("#outdoor-cat2").show();
-    $("#outdoor-cat3").show();
+    
 });
 let electronicsButton = document.getElementById("electronicsButton").addEventListener("click", function () {
     
@@ -103,23 +71,7 @@ let electronicsButton = document.getElementById("electronicsButton").addEventLis
     document.getElementById("electronicsButton").style.backgroundColor = "#154c33"; 
     //end
 
-    $("#t4").removeAttr('disabled');
-    $("label.t4").css("color", "black");
-    $("#book-cat1").hide();
-    $("#book-cat2").hide();
-    $("#book-cat3").hide();
-    $("#action-cat1").hide();
-    $("#action-cat2").hide();
-    $("#action-cat3").hide();
-    $("#game-cat1").hide();
-    $("#game-cat2").hide();
-    $("#game-cat3").hide();
-    $("#outdoor-cat1").hide();
-    $("#outdoor-cat2").hide();
-    $("#outdoor-cat3").hide();
-    $("#electronics-cat1").show();
-    $("#electronics-cat2").show();
-    $("#electronics-cat3").show();
+    
 });
 let GamesAndPuzzlesButton = document.getElementById("GamesAndPuzzlesButton").addEventListener("click", function () {
     
@@ -143,23 +95,7 @@ let GamesAndPuzzlesButton = document.getElementById("GamesAndPuzzlesButton").add
     document.getElementById("electronicsButton").style.backgroundColor = "#03a359"; 
     //end
 
-    $("#t4").removeAttr('disabled');
-    $("label.t4").css("color", "black");
-    $("#electronics-cat1").hide();
-    $("#electronics-cat2").hide();
-    $("#electronics-cat3").hide();
-    $("#action-cat1").hide();
-    $("#action-cat2").hide();
-    $("#action-cat3").hide();
-    $("#book-cat1").hide();
-    $("#book-cat2").hide();
-    $("#book-cat3").hide();
-    $("#outdoor-cat1").hide();
-    $("#outdoor-cat2").hide();
-    $("#outdoor-cat3").hide();
-    $("#game-cat1").show();
-    $("#game-cat2").show();
-    $("#game-cat3").show();
+    
 });
 let ActionFigures = document.getElementById("ActionFiguresButton").addEventListener("click", function () {
     //TOY_DATABASE=TOY_DATABASE.applyCategoryFilter("Action Figures");
@@ -183,21 +119,5 @@ let ActionFigures = document.getElementById("ActionFiguresButton").addEventListe
     document.getElementById("GamesAndPuzzlesButton").style.backgroundColor = "#2c9bce";
     document.getElementById("electronicsButton").style.backgroundColor = "#03a359";
     //end 
-    $("#t4").removeAttr('disabled');
-    $("label.t4").css("color", "black");
-    $("#electronics-cat1").hide();
-    $("#electronics-cat2").hide();
-    $("#electronics-cat3").hide();
-    $("#book-cat1").hide();
-    $("#book-cat2").hide();
-    $("#book-cat3").hide();
-    $("#game-cat1").hide();
-    $("#game-cat2").hide();
-    $("#game-cat3").hide();
-    $("#outdoor-cat1").hide();
-    $("#outdoor-cat2").hide();
-    $("#outdoor-cat3").hide();
-    $("#action-cat1").show();
-    $("#action-cat2").show();
-    $("#action-cat3").show();
+    
 });
